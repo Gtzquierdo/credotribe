d5898dd76b383e55bee5df04ea2af761cb3dfd0f
Giancarlo Tzquierdo <gtzquierdo@protonmail.com>
2022-08-04 19:58:47 -0400

    tech component done

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -3,10 +3,39 @@ import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
-const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+const Technologies = () => (
+  <Section id="tech">
+    <SectionDivider />
+    <br />
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>
+      I've worked with a wide variety of technologies in the web development
+      world.
+    </SectionText>
+    <List>
+      <ListItem>
+        <DiReact size="3rem" />
+        <ListContainer>
+          <ListTitle>Front End</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            Javascript, React, Next, HTML, CSS, SCSS, Bootstrap, Material-UI,
+            and more.
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiFirebase size="3rem" />
+        <ListContainer>
+          <ListTitle>Back End</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            Node, Express, MongoDB, and other databases.
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
+  </Section>
 );
 
 export default Technologies;
