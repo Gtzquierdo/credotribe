f18b518b3784a5366a0b9ff72c14009bd52dcc44
Giancarlo Tzquierdo <gtzquierdo@protonmail.com>
2022-08-04 19:33:04 -0400

    new portfolio updates

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,7 +5,19 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome to <br />
+        CredoTribe
+      </SectionTitle>
+      {/* // Value, bring value to community and companies */}
+      <SectionText> 
+        CredoTribe is my personal site that I created to learn React.js and showcase my many different projects. I hope you enjoy my site.
+      </SectionText>
+      <Button onClick={() => window.location = 'https://google.com'}> Learn More </Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
