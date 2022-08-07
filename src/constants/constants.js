5b707a35274d3963e30f82dbe009dded23f20cc4
Giancarlo Tzquierdo <gtzquierdo@protonmail.com>
2022-08-07 17:45:44 -0400

    updated projects

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,10 +1,10 @@
 export const projects = [
   {
-    title: "MERN Memories",
+    title: "CredoTribe Blog",
     description:
-      "Using React, Node.js, Express & MongoDB you'll learn how to build a Full Stack MERN Application - from start to finish. The App is called Memories and it is a simple social media app that allows users to post interesting events that happened in their lives.",
-    image: "/images/1.png",
-    tags: ["Mongo", "Express", "React", "Node"],
+      "Using React, Next.js, and GraphQL, I created a blog for CredoTribe.com. The blog is an application where you can follow me along my journey and learn about my work. Mostly programming related, but I also have a lot of fun with my family.",
+    image: "/public/images/credoTribeBlog.jpg",
+    tags: ["React", "Next.js", "GraphQL"],
     source: "https://google.com",
     visit: "https://google.com",
     id: 0,
@@ -13,38 +13,38 @@ export const projects = [
     title: "E-Commerce",
     description:
       "Built an e-commerce site using React, Next.js and utilized Stripe for payment processing. The site is a single page application that allows users to view products, add them to their cart, and checkout.",
-    image: "/images/greatFind.png",
+    image: "/public/images/the_great_find.jpg",
     tags: ["React", "JavaScript"],
     source: "https://github.com/Gtzquierdo/the-great-find",
     visit: "https://google.com",
     id: 1,
   },
   {
-    title: "WebRTC App",
+    title: "HomeSky Inc.",
     description:
-      "This is a code repository for the corresponding YouTube video. In this tutorial, we're going to build and deploy a React Video Chat Application using WebRTC.",
-    image: "/images/3.jpg",
-    tags: ["React", "WebRTC"],
+      "Revamped website for HomeSky Inc. using HTML, CSS, Bootstrap, React, and Javascript. The is site is an overhaul of their previous site where customers can view more information about the company and their services.",
+    image: "/public/images/home_sky.jpg",
+    tags: ["React", "JavaScript", "Bootstrap"],
     source: "https://google.com",
     visit: "https://google.com",
     id: 2,
   },
-  {
-    title: "Unichat",
-    description:
-      "This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application",
-    image: "/images/4.jpg",
-    tags: ["React", "ChatEngine", "Firebase"],
-    source: "https://google.com",
-    visit: "https://google.com",
-    id: 3,
-  },
+  // {
+  //   title: "Unichat",
+  //   description:
+  //     "This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application",
+  //   image: "/images/4.jpg",
+  //   tags: ["React", "ChatEngine", "Firebase"],
+  //   source: "https://google.com",
+  //   visit: "https://google.com",
+  //   id: 3,
+  // },
 ];
 
 export const TimeLineData = [
-  { year: 2017, text: 'Started my journey', },
-  { year: 2018, text: 'Worked as a freelance developer', },
-  { year: 2019, text: 'Founded JavaScript Mastery', },
-  { year: 2020, text: 'Shared my projects with the world', },
-  { year: 2021, text: 'Started my own platform', },
+  { year: 2018, text: 'Started my journey', },
+  { year: 2019, text: 'Completed the Full Stack Web Dev Boot Camp', },
+  { year: 2020, text: 'Built projects to hone in skills', },
+  { year: 2021, text: 'Life happened and a detour had to be taken', },
+  { year: 2022, text: 'Returned to Web Dev with a PURPOSE, started freelancing business', },
 ];
